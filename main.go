92a8ff5198ebf09e3e23be6b0cd8ca59e2473e7a
pwieleba <piotrwieleba8@gmail.com>
2023-11-04 17:42:58 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 type CreditCard struct {
-	ID      int    `json:"id"`
+	ID      string `json:"id"`
 	Balance int    `json:"balance"`
 	Owner   *Owner `json:"owner"`
 }
@@ -20,9 +20,9 @@ type Owner struct {
 }
 
 var CCs = []CreditCard{
-	{ID: 2446, Balance: 1345141, Owner: &Owner{firstname: "ex1", lastname: "ex1.2", age: 45, address: "NY"}},
-	{ID: 96, Balance: 624262, Owner: &Owner{firstname: "ex2", lastname: "ex2.2", age: 69, address: "FL"}},
-	{ID: 245, Balance: 4626262, Owner: &Owner{firstname: "ex2", lastname: "ex3.2", age: 33, address: "WA"}},
+	{ID: "2446", Balance: 1345141, Owner: &Owner{firstname: "ex1", lastname: "ex1.2", age: 45, address: "NY"}},
+	{ID: "96", Balance: 624262, Owner: &Owner{firstname: "ex2", lastname: "ex2.2", age: 69, address: "FL"}},
+	{ID: "245", Balance: 4626262, Owner: &Owner{firstname: "ex2", lastname: "ex3.2", age: 33, address: "WA"}},
 }
 
 func getOwner(c *gin.Context) {
@@ -31,9 +31,9 @@ func getOwner(c *gin.Context) {
 }
 
 func getOwnerById(c *gin.Context) {
-	id = c.Param("ID")
+	ID := c.Param("ID")
 	for _, a := range CCs {
-		if a.ID == id {
+		if a.ID == ID {
 			c.IndentedJSON(http.StatusOK, a)
 		}
 	}
