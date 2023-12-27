5fee988f6a4b7cdc939deb3b27b527fed7be7878
pwieleba <piotrwieleba8@gmail.com>
2023-12-27 23:57:05 +0100

    repaired version/docker
    malfuncioned funtion getting by id destoyed whole program (?)  / deleting docker files (not in use)

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -31,18 +30,19 @@ func getOwner(c *gin.Context) {
 
 }
 
-func errormes() {
-	fmt.Println("User not found!")
-}
+//func errormes() {
+//	fmt.Println("User not found!") //used in func below
+//}
 
-func getOwnerById(c *gin.Context) {
-	ID := c.Param("ID")
-	for _, a := range CCs {
-		if a.ID == ID {
-			c.IndentedJSON(http.StatusOK, a)
-		} else if c.IndentedJSON(http.StatusBadRequest, errormes)
-	}
-}
+//func getOwnerById(c *gin.Context) {
+//ID := c.Param("ID")
+//for _, a := range CCs {
+//if a.ID == ID {
+//c.IndentedJSON(http.StatusOK, a)
+//	} //else if c.IndentedJSON(http.StatusBadRequest, errormes);
+// }
+
+//} //code doesnt work when this func is in it?????????
 
 func createOwner(c *gin.Context) {
 	var newOwner CreditCard
@@ -58,7 +58,7 @@ func createOwner(c *gin.Context) {
 func deleteOwner(c *gin.Context) {
 	ID := c.Param("ID")
 	client := &http.Client{}
-	req, err := http.NewRequest("DELETE", "http://localhost:8080/CCs/"+ ID, nil)
+	req, err := http.NewRequest("DELETE", "http://localhost:8080/CCs/"+ID, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -75,11 +75,10 @@ func deleteOwner(c *gin.Context) {
 }
 
 func main() {
-
 	r := gin.Default()
-	r.GET("/CCs", getOwner)         //works
-	r.GET("/CCs/:ID", getOwnerById) //works
-	r.POST("/CCs", createOwner)     //works
+	r.GET("/CCs", getOwner) //works
+	//r.GET("/CCs/:ID", getOwnerById) //do not works
+	r.POST("/CCs", createOwner) //works
 	// r.PUT("/CCs/:ID", updateOwnerData) //nw
 	r.DELETE("/CCs/:ID", deleteOwner) //?
 	r.Run("localhost:8080")
